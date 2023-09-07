00611daa13757c53317c48757c0a250147b997e3
ilya Pidonenko <ilja.pidonekom@gmail.com>
2023-09-07 13:02:09 +0200

    import update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import hh from "hyperscript-helpers";
 import { h, diff, patch } from "virtual-dom";
 import createElement from "virtual-dom/create-element";
 
-const { div, button, p, h1, input, table, tr, td, th, span } = hh(h);
+const { div, button, p, h1, input } = hh(h);
 
 const btnStyle = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
 
@@ -18,7 +18,7 @@ const MSGS = {
 function view(dispatch, model) {
   return div({ className: "flex flex-col gap-4 items-center" }, [
     h1({ className: "text-2xl" }, "Karteikartenspiel"),
-    
+
     div({ className: "flex gap-4" }, [
       input({
         type: "text",
@@ -40,18 +40,16 @@ function view(dispatch, model) {
       }),
       button({ className: btnStyle, onclick: () => dispatch({ type: MSGS.ADD_CARD }) }, "Hinzufügen"),
     ]),
-    
+
     div({ className: "flex flex-wrap w-full" }, model.cards.map((card, index) =>
-      div({ className: "w-1/3 p-4 border" }, [ // Jede Karte nimmt 1/3 der Breite ein und hat einen Rand
+      div({ className: "w-1/3 p-4 border" }, [
         button({ className: "text-red-500 float-right", onclick: () => dispatch({ type: MSGS.DELETE_CARD, index }) }, "Löschen"),
         div({ className: "clear-both" }),
-        p("Frage: " + card.question),
-        p({
-          className: "cursor-pointer",
+        p({ className: "bg-blue-100 p-2" }, "Frage: " + card.question),
+        button({
           onclick: () => dispatch({ type: MSGS.TOGGLE_ANSWER, index }),
         }, card.showAnswer ? "Antwort ausblenden" : "Antwort einblenden"),
-        card.showAnswer ? p("Antwort: " + card.answer) : null,
-        // Bewertungsknöpfe unter der Antwort
+        card.showAnswer ? p({ className: "bg-blue-100 p-2" }, "Antwort: " + card.answer) : null,
         card.showAnswer ? div({ className: "mt-2" }, [
           button({ className: "text-red-500", onclick: () => dispatch({ type: MSGS.RATE_CARD, index, rating: 0 }) }, "Schlecht"),
           button({ className: "text-blue-500 ml-2", onclick: () => dispatch({ type: MSGS.RATE_CARD, index, rating: 1 }) }, "Gut"),
@@ -112,7 +110,6 @@ function app(initModel, update, view, node) {
   let currentView = view(dispatch, model);
   let rootNode = createElement(currentView);
   node.appendChild(rootNode);
-
   function dispatch(msg) {
     model = update(msg, model);
     const updatedView = view(dispatch, model);
